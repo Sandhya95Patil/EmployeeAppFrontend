3e9507facb8444a35a26068abe22dae149f7c21b
Sandhya Patil <sandhyapatil364@gmail.com>
2020-08-11 17:54:26 +0530

    Added: UI of employee app

---
+++ src/Components/SignUpComponents.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class SignUpComponents extends Component{
+render(){
+    return <div>Hello</div>
+}
+}
+export default SignUpComponents
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HomeComponent from './Components/HomeComponent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeComponent name="sandhya"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
